8cacb35de9d5d1e75372903bc8d39a1d44aa76d5
Borworn Kultumyotin <177917303+Byffify@users.noreply.github.com>
2025-11-29 22:15:18 +0700

    update practice week13

---
+++ week13/practice1/main.js
@@ -0,0 +1,41 @@
+const postList = document.getElementById("postList")
+const loadMoreBtn = document.getElementById("loadMoreBtn")
+const doneMsg = document.getElementById("doneMsg")
+
+let currentPage = 1
+const limit = 2
+const maxPosts = 10
+
+async function loadPosts() {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${limit}`
+    );
+    const data = await response.json()
+
+    data.forEach((post) => {
+      const div = document.createElement("div")
+      div.className = "post"
+      div.textContent = `${post.id}: ${post.title}`
+      postList.appendChild(div)
+    })
+
+    currentPage++
+
+    if ((currentPage - 1) * limit >= maxPosts) {
+      loadMoreBtn.style.display = "none"
+      doneMsg.textContent = "load finish"
+    }
+
+    if (data.length < limit) {
+      loadMoreBtn.style.display = "none"
+      doneMsg.textContent = "Not more load"
+    }
+  } catch (error) {
+    doneMsg.textContent = "Error kubbb"
+    console.error(error)
+  }
+}
+
+loadPosts()
+loadMoreBtn.addEventListener("click", loadPosts)
